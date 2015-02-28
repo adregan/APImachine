a9cb31682b3b386bcc873b2db9ac3ad0b7b76930
Duncan Regan <duncanregan@gmail.com>
2015-02-28 14:55:31 -0500

    import the handlers

--- config/endpoints.py
+++ config/endpoints.py
@@ -2,6 +2,8 @@
     The endpoints are a dictionary of routes and handlers which the app will
     use to construct the routes.
 """
+from src.handlers import *
+
 
 endpoints = [
     {"route": "", "handler": ""}
