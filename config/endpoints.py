2537de9f0e8559182bf9213843e821b18ad7f0da
Duncan Regan <duncanregan@gmail.com>
2015-02-28 14:52:40 -0500

    Adds comments and the endpoints object

---
+++ config/endpoints.py
@@ -0,0 +1,8 @@
+""" Configure your endpoints here.
+    The endpoints are a dictionary of routes and handlers which the app will
+    use to construct the routes.
+"""
+
+endpoints = [
+    {"route": "", "handler": ""}
+]
