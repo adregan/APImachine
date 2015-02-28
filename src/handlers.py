3523c6a0da90a147cedcf7f8d098c36b40379bf7
Duncan Regan <duncanregan@gmail.com>
2015-02-28 14:59:51 -0500

    Scaffolding our methods

--- src/handlers.py
+++ src/handlers.py
@@ -3,3 +3,18 @@ import tornado.web
 class DefaultHandler(tornado.web.RequestHandler):
     def get(self):
         self.write("Hello")
+
+    def post(self):
+        pass
+
+    def patch(self):
+        pass
+
+    def put(self):
+        pass
+
+    def delete(self):
+        pass
+
+    def options(self):
+        pass
