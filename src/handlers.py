4ba12037b7d6cda4febc54883c35a6d3681722b1
Duncan Regan <duncanregan@gmail.com>
2015-02-28 15:01:41 -0500

    Sets default headers

--- src/handlers.py
+++ src/handlers.py
@@ -1,6 +1,11 @@
 import tornado.web
 
 class DefaultHandler(tornado.web.RequestHandler):
+    def set_default_headers(self):
+        self.set_header("Access-Control-Allow-Origin", "*")
+        self.set_header("Access-Control-Allow-Headers", "Content-Type")
+        self.set_header("Content-Type", "application/json")
+
     def get(self):
         self.write("Hello")
 
