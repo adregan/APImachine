6165ef408f95b254473193b00eb67c5bf8004abc
Duncan Regan <duncanregan@gmail.com>
2015-02-28 20:11:19 -0500

    Adds a config for defining schemas
    uses marshmallow

---
+++ config/schemas.py
@@ -0,0 +1,11 @@
+from marshmallow import Schema, fields
+
+""" Define your collections' schemas here for validation and marshalling
+"""
+
+class TestSchema(Schema):
+    title = fields.String(default="Untitled")
+    type = fields.Select(
+        choices=["fruit", "vegetable", "meat"],
+        default="fruit"
+    )
