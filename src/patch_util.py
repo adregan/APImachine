22d95fd80395352a32ac24c25259c3b128992e6e
Duncan Regan <duncanregan@gmail.com>
2015-03-14 15:46:24 -0400

    Creates a key validator to ensure that the proper keys are present on patches

--- src/patch_util.py
+++ src/patch_util.py
@@ -9,4 +9,30 @@ class Patchy(object):
             self.patch_requests = [self.patch_requests]
 
     def validate(self):
+        key_errors = self._validate_patch_keys()
+        print(key_errors)
         return None
+
+    def _validate_patch_keys(self):
+        op_errors = [
+            {'index': i + 1, 'error': 'missing key `op`'}
+            for i, request in enumerate(self.patch_requests)
+            if 'op' not in request.keys()
+        ]
+        path_errors = [
+            {'index': i + 1, 'error': 'missing key `path`'}
+            for i, request in enumerate(self.patch_requests)
+            if 'path' not in request.keys()
+        ]
+        value_errors = [
+            {'index': i + 1, 'error': 'missing key `value`'}
+            for i, request in enumerate(self.patch_requests)
+            if 'value' not in request.keys()
+        ]
+
+        combined_errors = op_errors + path_errors + value_errors
+
+        key_errors = self._build_error_messages(combined_errors)
+
+        return key_errors
+
