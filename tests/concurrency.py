712d74e008070b06e7bb554d04ff0688eb5754d6
Duncan Regan <duncanregan@gmail.com>
2015-03-21 16:33:22 -0400

    creates a concurrency test for POST

--- tests/concurrency.py
+++ tests/concurrency.py
@@ -2,6 +2,7 @@
 import click
 import requests
 import time
+import json
 from multiprocessing import Pool
 from multiprocessing.dummy import Pool as ThreadPool
 
@@ -37,9 +38,39 @@ def get(url, number):
 
     return
 
+@cli.command()
+@click.argument('url', metavar='URL', required=True)
+@click.option('--number', default=10, help='The number of requests to make.')
+def post(url, number):
+    start = time.time()
+    urls = [url for i in range(0, number)]
+    pool = ThreadPool(number)
+    # Map over the media
+    response_times = pool.map(post_request, urls)
+    # Close the pool
+    pool.close()
+    pool.join()
+
+    for resp_time in response_times:
+        print(resp_time)
+
+    total_seconds = int(time.time() - start)
+    seconds = total_seconds % 60
+
+    print('TOTAL time {seconds} sec'.format(seconds=seconds))
+
+    return
 
 def get_request(url):
     resp = requests.get(url)
+    if resp.status_code >= 300:
+        return 'Error: {code}'.format(code=resp.status_code)
+    return resp.elapsed
+
+def post_request(url):
+    resp = requests.post(url, data=json.dumps({}))
+    if resp.status_code >= 300:
+        return 'Error: {code}'.format(code=resp.status_code)
     return resp.elapsed
 
 if __name__ == '__main__':
