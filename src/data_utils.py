66408e0b1af5b8d5d4ac62ff5ef51c765724579a
Duncan Regan <duncanregan@gmail.com>
2015-03-22 22:46:47 -0400

    utils allow for singles

--- src/data_utils.py
+++ src/data_utils.py
@@ -1,16 +1,28 @@
-def convert_database_cursor(cursor):
-    converted = [
-        dict(list(
-            zip([col[0] for col in cursor.description], row)
-        )) for row in cursor.fetchall()
-    ]
+def convert_database_cursor(cursor, single):
+    keys = [col[0] for col in cursor.description]
+    if single:
+        results = cursor.fetchone()
+        if not results:
+            return
+        converted = dict(zip(keys, results))
+
+    else:
+        results = cursor.fetchall()
+        converted = [
+            dict(list(
+                zip(keys, values)
+            )) for values in results
+        ]
 
     return converted
 
 def convert_to_json(schema, cursor_objects):
-    data = [
-        schema().dump(object).data 
-        for object in cursor_objects
-    ]
+    if isinstance(cursor_objects, (list)):
+        data = [
+            schema().dump(object).data 
+            for object in cursor_objects
+        ]
+    else:
+        data = schema().dump(cursor_objects).data
 
     return data
